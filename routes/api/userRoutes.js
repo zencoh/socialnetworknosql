b431d6587b09759dbd6d17140ab9894f1667b6aa
Cole Hartkopp <chsoccer4@outlook.com>
2023-04-17 14:12:58 -0500

    update and remove user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 const {
     // controllers
     getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers);
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
